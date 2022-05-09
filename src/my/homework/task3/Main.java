ce3210cc1598e7d88070a727fb6f91e0df1ccce5
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-05-09 03:54:50 +0300

    first homework

---
+++ src/my/homework/task3/Main.java
@@ -0,0 +1,22 @@
+package my.homework.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        byte b = 85;
+        System.out.println("b = " + b);
+        short s = 22015;
+        System.out.println("s = " + s);
+        int i = 1000000;
+        System.out.println("i = " + i);
+        long l = 4294967295L;
+        System.out.println("l = " + l);
+        char c = 'a';
+        System.out.println("c = " + c);
+        float f = 0.25F;
+        System.out.println("f = " + f);
+        double d = 1.234E-5D;
+        System.out.println("d = " + d);
+        boolean bool = true;
+        System.out.println("bool = " + bool);
+    }
+}
