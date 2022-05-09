8657335a3f0cb37f365a7ccc24d59acbee0dfef8
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-05-09 22:47:03 +0300

    Enum with description of seasons of year

---
+++ src/my/homework/task35/Main.java
@@ -0,0 +1,11 @@
+package my.homework.task35;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Seasons of the year: ");
+        for (Seasons seasons : Seasons.values()) {
+            System.out.println(seasons);
+        }
+            }
+}
+
---
+++ src/my/homework/task35/Seasons.java
@@ -0,0 +1,11 @@
+package my.homework.task35;
+
+public enum Seasons {
+    SUMMER,
+    SPRING,
+    AUTUMN,
+    WINTER;
+
+    String description = "Depending on the season on the outside is hot, rainy, snowy or windy.";
+    int countOfDays;
+}
